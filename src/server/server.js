261289a03f5a2277674e3f1da1e5676d613b7ee1
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-04 21:28:27 +0100

    New server file
    feat: new file
    
    body: new server for new API

---
+++ src/server/server.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser')
+ // Express to run server and routes
+const express = require('express');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+//configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 8081;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`im listening the port: ${port}`)
+}
+

6ef680cbb3875e0ecf2edac26719c13f41394ada
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-10 17:20:33 +0100

    Adding Fetch
    type: adding Fetch 
    
    body: Working to send the data from client to server.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,25 @@
-function handleSubmit(event) {
+const postData = async (url = '', data)=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+ 
+ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -6,12 +27,17 @@ function handleSubmit(event) {
 
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
+    let name = document.getElementById('results').innerHTML;
+
+    postData('http://localhost:8080/test', {name})
+
+ /*     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
-    })
+    })  */
+
 }
 
-export {handleSubmit};
+export {handleSubmit, postData};

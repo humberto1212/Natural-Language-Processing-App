8899abb93ea0746052304a010b0ce83ff86d10d7
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-17 23:26:14 +0100

    Update app.post
    Some changes were made to (app.post('/api') to be able to send and receive inf. from MeaningCloud

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
  // Express to run server and routes
 const express = require('express');
@@ -11,6 +12,17 @@ const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key="
 const apiKey = process.env.API_KEY;
 console.log(`Your API key is ${process.env.API_KEY}`);
 
+
+const meaning = async (url, key, para) => {
+    const res = await fetch (url+key+'&of=json&txt='+para+'.&lang=en')
+    try{
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }catch(error){
+        console.log("error", error)
+    }
+}
 
 //configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,14 +42,9 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html');
 });
 
-let data = [];
-app.post('/test', function (req, res){
-    let newData = req.body;
-    let newEntry = {
-        name: newData.name
-    }
-    console.log(newEntry)
-    data.unshift(newEntry);
-    res.send(data);
-    console.log(data)
+app.post('/api', function (req, res){
+    const text = req.body.text;
+    const para = text
+    console.log(para);
+    meaning(baseUrl,apiKey,para)
 });

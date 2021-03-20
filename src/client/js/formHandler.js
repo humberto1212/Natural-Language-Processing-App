78a5f4a39b964af7574a8db8932e5c7ed3010df1
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-21 00:10:35 +0100

    New features
    five new 'const' were added and some change in the postData were made

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -17,8 +17,7 @@ const postData = async (url = '', data = {})=>{
       }catch(error) {
       console.log("error", error);
       }
-  }
- 
+  }  
 
  function handleSubmit(event) {
   event.preventDefault()
@@ -26,13 +25,20 @@ const postData = async (url = '', data = {})=>{
   // check what text was put into the form field
   let formText = document.getElementById('name').value
 
-  Client.checkForName(formText)
-  
+  // UI update as result of the analysis
+  const score = "(P+) Strong Positive | (P) Positive | (NEU) Neutral | (N) Negative | (N+) Strong Negative: ";
+  const agree = "Agreement or Disagreement: ";
+  const objective = "Objective or Subjective: ";
+  const confidenceLevel = "Confidence: ", porcent = " %";
+  const irony = "Nonironic or Ironic: ";
+
   postData('http://localhost:8081/api', {text: formText})
   .then(function(res) {
-        document.getElementById('results').innerHTML = res
+    document.getElementById('scoreTag').innerHTML = score + res.score_tag;
+    document.getElementById('agreement').innerHTML = agree + res.agreement;
+    document.getElementById('subjectivity').innerHTML = objective + res.subjectivity;
+    document.getElementById('confidence').innerHTML = confidenceLevel + res.confidence + porcent;
+    document.getElementById('irony').innerHTML = irony + res.irony;
   })
-
 }
-
 export {handleSubmit};

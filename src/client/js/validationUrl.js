fba1e37fdbe873e356ad79c20d2fc0f9d9a505c4
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-24 11:53:18 +0100

    Update validationUrl.js

--- src/client/js/validationUrl.js
+++ src/client/js/validationUrl.js
@@ -1,12 +1,13 @@
-function validURL(url) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(url);
+function validURL(str) {
+    var validation = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+      if (validation.test(str))
+      {
+        return true;
+      }
+      else
+      {
+        return null;
+      }
   }
-
-export { validURL }
-
+  
+  export { validURL };

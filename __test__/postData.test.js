236f322cf1075ffba70be59d123a9d6c61d11c7a
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-03-28 21:14:04 +0200

    added new test file
    added postData.test file

---
+++ __test__/postData.test.js
@@ -0,0 +1,8 @@
+import {postData} from '../src/client/js/formHandler'
+
+describe("Testing the URL validation", () => {
+    test("The url hast to be true", () => {
+           expect(postData).toBeDefined();
+    })
+}); 
+

ddad43852891385e1f0161896410da03c50e6bd5
Humberto Angelucci <71103179+humberto1212@users.noreply.github.com>
2021-04-04 12:24:44 +0200

    feat: Add loaders and plugins
    Added a plugin and loader to minimize the space generated by the styles files when the production environment is executed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,20 @@
-const path = require('path')
-const webpack = require('webpack')
-const htmlWebPackPlugin = require('html-webpack-plugin')
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
         library: 'Client'
-        },
-    mode: 'production',
+    },
+    optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
                 {
@@ -18,14 +24,15 @@ module.exports = {
                 },
                 {
             test: /\.scss$/,
-            use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
                 }
-        ]
+        ]  
     },
     plugins: [
-        new htmlWebPackPlugin({
+        new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
+    ]      
 }
